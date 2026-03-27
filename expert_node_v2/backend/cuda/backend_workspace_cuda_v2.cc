272b09d8f4b28012f48e3ac0aa4b95069a3742f9
zhang3 <shuai@zhang3lab.work>
2026-03-28 00:07:04 +0800

    vvvv

--- expert_node_v2/backend/cuda/backend_workspace_cuda_v2.cc
+++ expert_node_v2/backend/cuda/backend_workspace_cuda_v2.cc
@@ -1,6 +1,24 @@
-#include "expert_node_v2/backend/cuda/backend_workspace_cuda_v2_internal.h"
+#include "expert_node_v2/backend/cuda/backend_workspace_cuda_v2.h"
+
+#include <memory>
+#include <type_traits>
+
+#include <cuda_fp16.h>
+#include <cuda_runtime.h>
+#if EXPERT_NODE_V2_HAS_CUDA_BF16
+#include <cuda_bf16.h>
+#endif
+
+#include "expert_node_v2/backend/cuda/backend_cuda_v2.h"
 
 namespace expert_node_v2 {
+
+class BackendWorkspaceCudaV2::Impl {
+public:
+    int local_gpu_id = -1;
+    ExpertWorkspaceCudaV2 ws{};
+    bool ok = false;
+};
 
 namespace {
 
